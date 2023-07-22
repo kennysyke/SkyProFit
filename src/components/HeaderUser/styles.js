d49aff60792ee1cab120e586a639a36f1b7ce6b2
msr <mar_r@mail.ru>
2023-07-22 10:49:11 +0300

    fix: добавлен редирект при выходе

--- src/components/HeaderUser/styles.js
+++ src/components/HeaderUser/styles.js
@@ -20,25 +20,46 @@ export const Email = Styled.div`
 export const Popup = Styled.div`
     position: absolute;
     top: 100%;
-    margin-top: 10px;
     right: 0;
-    min-width: 100%;
+    min-width: 160px;
     font-size: 16px;
     white-space: nowrap;
     border-radius: 10px;
     padding: 20px;
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    gap: 20px;
     box-shadow: 0 0 15px rgba(0,0,0,0.2);
 `
 
 export const NavLink = Styled(Link)`
     color: #000;
+    align-self: flext-start;
 `
-
 export const ArrowIcon = Styled.svg`
     cursor: pointer;
     transition: transform 0.3s ease;
     transform: ${({ isOpen }) => (isOpen ? 'rotate(-180deg)' : 'none')};
+`
+
+export const Button = Styled.button`
+  display: block;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  transition: all .2s ease;
+  border-radius: 46px;
+  letter-spacing: -0.05px;
+  min-width: 78px;
+  padding: 5px 16px 7px;
+  background: #580ea2;
+  color: #FFF;
+
+  &:hover {
+    background:#3F007D;
+  }
+
+  &:active {
+    background:#271A58;
+  }
 `
