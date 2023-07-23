46f5bc4db01ffd1690904ff3732f27f87ce2d956
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-07-23 15:04:56 +0300

    rename file

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,11 +1,12 @@
-import { AppRoutes } from "../../routes";
-import * as Styled from "./styles";
+import { AppRoutes } from '../../routes'
+import * as Styled from './styles'
 
 export function App() {
-  
   return (
     <Styled.Wrapper>
       <AppRoutes />
     </Styled.Wrapper>
-  );
+  )
 }
+
+// changes for git to rename the file
