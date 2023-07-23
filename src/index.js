8147aae2285b3835f9714ff770b23d4e23f9ef6f
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-07-23 10:27:23 +0300

    small changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store'
-import { App } from './components/app/App'
+import { App } from './components/app/app'
 import { GlobalStyle } from './components/app/globalStyles'
 import './firebase'
 
